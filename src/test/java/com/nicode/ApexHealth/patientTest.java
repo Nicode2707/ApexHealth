8a23f5f3f6ad7479e6a83b6643378d1b0e2c7d26
Niraj Kumar <nirajkr2707@gmail.com>
2026-07-31 16:38:02 +0530

    Add jpa method and jpa query

--- src/test/java/com/nicode/ApexHealth/patientTest.java
+++ src/test/java/com/nicode/ApexHealth/patientTest.java
@@ -2,9 +2,13 @@ package com.nicode.ApexHealth;
 
 import com.nicode.ApexHealth.Entity.patient;
 import com.nicode.ApexHealth.Repository.patientRepository;
+import com.nicode.ApexHealth.Service.PatientService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 
 import java.util.List;
 
@@ -14,11 +18,55 @@ public class patientTest {
     @Autowired
     private patientRepository patientRepository;
 
+    @Autowired
+    private PatientService patientService;
+
     @Test
     public void testpatientRepository() {
         List<patient> patientList = patientRepository.findAll();
         System.out.println(patientList);
+    }
+
+    @Test
+    public void testPatientRepository() {
+
+        List<patient> patientList = patientRepository.findAllPatientWithAppointment();
+        System.out.println(patientList);
 
     }
 
+    @Test
+    public void testTransactionMethods() {
+//        Patient patient = patientService.getPatientById(1L);
+
+//        Patient patient = patientRepository.findById(1L).orElseThrow(() -> new EntityNotFoundException("Patient not " +
+//                "found with id: 1"));
+
+//        Patient patient = patientRepository.findByName("Diya Patel");
+
+//        List<Patient> patientList = patientRepository.findByBirthDateOrEmail(LocalDate.of(1988, 3, 15), "diya" +
+//                ".patel@example.com");
+
+//        List<Patient> patientList = patientRepository.findByBornAfterDate(LocalDate.of(1993, 3, 14));
+
+        Page<patient> patientList = patientRepository.findAllPatients(PageRequest.of(1, 2, Sort.by("name")));
+
+        for (patient patient : patientList) {
+            System.out.println(patient);
+        }
+//
+//        List<Object[]> bloodGroupList = patientRepository.countEachBloodGroupType();
+//        for(Object[] objects: bloodGroupList) {
+//            System.out.println(objects[0] +" "+ objects[1]);
+//        }
+
+//        int rowsUpdated = patientRepository.updateNameWithId("Arav Sharma", 1L);
+//        System.out.println(rowsUpdated);
+
+//        List<BloodGroupCountResponseEntity> bloodGroupList = patientRepository.countEachBloodGroupType();
+//        for(BloodGroupCountResponseEntity bloodGroupCountResponse: bloodGroupList) {
+//            System.out.println(bloodGroupCountResponse);
+//        }
+
+    }
 }
